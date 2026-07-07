b36c5e0e32fc5b29a944f33a1523e64754bc2a18
Tito Duarte <tito.duarte@interligent.com>
2026-07-07 14:26:54 +0100

    2.3.10

--- src/commands/upgrade.rs
+++ src/commands/upgrade.rs
@@ -5,12 +5,6 @@ use std::process::Command;
 const TAP_FORMULA: &str = "INTERLIGENT-kommunzieren-GmbH/tap/docker-control";
 
 pub fn execute() -> Result<()> {
-    ui::info("Updating Homebrew...");
-    let status = Command::new("brew").arg("update").status()?;
-    if !status.success() {
-        bail!("brew update failed with status {}", status);
-    }
-
     ui::info(format!("Upgrading {}...", TAP_FORMULA));
     let status = Command::new("brew")
         .args(["upgrade", TAP_FORMULA])
